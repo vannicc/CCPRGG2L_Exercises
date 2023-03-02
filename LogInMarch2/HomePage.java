619e1fa35c6e94a7905dfb61976351a8ad28ceed
Jerico Lim <111488202+vannicc@users.noreply.github.com>
2023-03-02 10:09:35 +0800

    Create HomePage.java

---
+++ LogInMarch2/HomePage.java
@@ -0,0 +1,53 @@
+package SWING;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
+public class HomePage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomePage frame = new HomePage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomePage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 128, 255));
+		contentPane.setForeground(new Color(0, 128, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHomePage = new JLabel("WELCOME USER!");
+		lblHomePage.setForeground(Color.WHITE);
+		lblHomePage.setFont(new Font("Tahoma", Font.BOLD, 50));
+		lblHomePage.setBounds(173, 141, 479, 128);
+		contentPane.add(lblHomePage);
+	}
+
+}
